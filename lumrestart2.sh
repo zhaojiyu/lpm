763ac068585e4cab2c064d849cb6993fb2e1f744
zhaojiyu <38657085+zhaojiyu@users.noreply.github.com>
2021-02-07 17:23:57 +0800

    Update lumrestart2.sh

--- lumrestart2.sh
+++ lumrestart2.sh
@@ -12,14 +12,11 @@ else
     echo "Kill OK..."
 fi
 sleep 3s
-luminati -d
+luminati -d --www_whitelist_ips "47.75.49.75"
 echo "Waiting luminati start..."
 sleep 10s
 rm -rf iptables* 
 iptables -F
 iptables -P INPUT ACCEPT
 iptables -L -n
-lpm_whitelist_ip 47.75.49.75
-lpm_whitelist_ip 121.196.8.110
-lpm_whitelist_ip 222.174.185.118
 echo "Luminati Start OK..."
