c8a3050de91b427016bae3a9f40c45dc09259e9f
zhaojiyu <38657085+zhaojiyu@users.noreply.github.com>
2022-03-01 16:26:18 +0800

    Add files via upload

---
+++ gost2.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+lum_id=`ps -ef|grep gost|grep -v grep|awk '{print $2}'`
+if  [ ! -n "$lum_id" ] ; then
+    echo "will start..."
+else
+    echo $lum_id
+    kill -9 $lum_id
+    echo "Kill OK"
+fi
+sleep 1s
+
+nohup ./gost -L=tcp://:1433/asia1.ethermine.org:4444 -F=relay+tls://47.75.49.75:8443 > log8.log 2>&1 &
+
+echo "Gost 2 Start OK......"
