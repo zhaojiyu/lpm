ac42d5d5bf0e9c3222b66e84a76215d042149e2b
zhaojiyu <38657085+zhaojiyu@users.noreply.github.com>
2022-03-01 17:21:30 +0800

    Update agost.sh

--- agost.sh
+++ agost.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 rm -rf *.log
 chmod +x gost
-lum_id=`ps -ef|grep gost|grep -v grep|awk '{print $2}'`
-if  [ ! -n "$lum_id" ] ; then
+id=`ps -ef|grep gost|grep -v grep|awk '{print $2}'`
+if  [ ! -n "$id" ] ; then
     echo "will start..."
 else
-    echo $lum_id
-    kill -9 $lum_id
+    echo "Find ID:"
+    echo $id
+    kill -9 $id
     echo "Kill OK"
 fi
 sleep 1s
-
 nohup ./gost -L=tcp://:1433/asia1.ethermine.org:4444 -F=relay+tls://47.75.49.75:8443 > log8.log 2>&1 &
-
 echo "Gost 2 Start OK......"
