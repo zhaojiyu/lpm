789a8c8e67fd92eef4a57ac1258442f1f665b485
zhaojiyu <38657085+zhaojiyu@users.noreply.github.com>
2022-03-01 16:57:06 +0800

    Update gost1.sh

--- gost1.sh
+++ gost1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget https://raw.githubusercontent.com/zhaojiyu/lpm/main/gost && sh chmod +x gost
+wget https://raw.githubusercontent.com/zhaojiyu/lpm/main/gost && chmod +x gost
 sleep 1s
 nohup ./gost -L=tcp://:1433/asia1.ethermine.org:4444 -F=relay+tls://47.75.49.75:8443 > log8.log 2>&1 &
 rm -rf gost1.sh
