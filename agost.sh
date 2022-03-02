a20067c2f6f3580b097a5e59128ac03f990f6f1f
zhaojiyu <38657085+zhaojiyu@users.noreply.github.com>
2022-03-02 17:19:25 +0800

    Update agost.sh

--- agost.sh
+++ agost.sh
@@ -2,5 +2,5 @@
 rm -rf *.log
 chmod +x gost
 sleep 1s
-nohup ./gost -L=tcp://:1433/asia1.ethermine.org:4444 -F=relay+tls://47.75.49.75:8443 > log8.log 2>&1 &
+nohup ./gost -L=tcp://:1433/asia1.ethermine.org:4444 -F=relay+tls://47.243.143.166:3306 > logs.log 2>&1 &
 echo "Gost Start OK......"
