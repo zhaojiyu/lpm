164b8d9e0042cbb47fc7961bb9a256648bc4922a
zhaojiyu <38657085+zhaojiyu@users.noreply.github.com>
2022-03-04 17:58:23 +0800

    Delete gost1.sh

--- gost1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-wget https://raw.githubusercontent.com/zhaojiyu/lpm/main/gost && chmod +x gost
-sleep 1s
-wget https://raw.githubusercontent.com/zhaojiyu/lpm/main/agost.sh
-rm -rf gost1.sh
-echo "Gost Install OK......"
