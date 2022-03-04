5b98917021db4cd0ae1e002e743bbb28633f8d10
zhaojiyu <38657085+zhaojiyu@users.noreply.github.com>
2022-03-04 17:58:34 +0800

    Delete agost.sh

--- agost.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-rm -rf *.log
-chmod +x gost
-sleep 1s
-nohup ./gost -L=tcp://:1433/asia1.ethermine.org:4444 -F=relay+tls://47.243.143.166:3306 > logs.log 2>&1 &
-echo "Gost Start OK......"
